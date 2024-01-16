1984397dea5a945ada52647d16f60aeeb6f6b42d
sunzu chris <sunzuchris250@gmail.com>
2024-01-16 20:29:50 +0200

    generating images from backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GetAllToursComponent } from './components/get-all-tours/get-all-tours.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GetAllToursComponent
+    GetAllToursComponent,
   ],
   imports: [
     BrowserModule,HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatCardModule, MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
