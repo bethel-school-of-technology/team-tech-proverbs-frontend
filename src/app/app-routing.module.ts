596daf829b2686790bf38dbd5b3985971a21d7be
Ange Gardien <angegirishyaka@gmail.com>
2024-01-17 22:53:46 +0200

    Generated SignUp/SignIn Component, User Service and User model

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GetAllToursComponent } from './components/get-all-tours/get-all-tours.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { UserSigninComponent } from './components/user-signin/user-signin.component';
 
 const routes: Routes = [
   {path: "", component: GetAllToursComponent},
-  {path: "allTours", component: GetAllToursComponent}
+  {path: "allTours", component: GetAllToursComponent},
+  {path: "signup", component: SignUpComponent},
+  {path: "login", component: UserSigninComponent}
 ];
 
 @NgModule({
