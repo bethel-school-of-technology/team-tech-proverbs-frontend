8b6db7fec92de6acd712a753843478a9587c878f
Ishkevin32 <ishimwekevin32@gmail.com>
2024-01-18 08:11:23 +0200

    Added HTML Overview of get-all-tours-component

--- src/app/components/get-all-tours/get-all-tours.component.ts
+++ src/app/components/get-all-tours/get-all-tours.component.ts
@@ -5,24 +5,17 @@ import { TourService } from 'src/app/services/tour.service';
 @Component({
   selector: 'app-get-all-tours',
   templateUrl: './get-all-tours.component.html',
-  styleUrls: ['./get-all-tours.component.css']
+  styleUrls: ['./get-all-tours.component.css'],
 })
 export class GetAllToursComponent implements OnInit {
+  TourList: Tour[] = [];
 
-  TourList:Tour[] = [];
-
-  constructor (private tourService: TourService) {}
+  constructor(private tourService: TourService) {}
 
   ngOnInit(): void {
-    this.tourService.getAllTours().subscribe(response => {
+    this.tourService.getAllTours().subscribe((response) => {
       this.TourList = response;
-      console.log(response[0].imageCover);
-    })
-  }
-
-  getImageUrl(imageName: string): string {
-    // Construct the full URL based on your backend server setup
-    // return `http://127.0.0.1:3004/public/img/tours/${imageName}`;
-    return `D:\finalProject\Rwandatours\public\img\tours\tour-2-cover.jpg`;
+      console.log(response[8].imageCover);
+    });
   }
 }
