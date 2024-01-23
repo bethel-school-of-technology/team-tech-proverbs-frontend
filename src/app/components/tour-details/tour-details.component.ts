6baec85ff411e43fed9fb9078f122f1b757e07ee
David Mugisha <davidkhalifa47@gmail.com>
2024-01-23 13:08:30 +0200

    Routing for tour-details page

--- src/app/components/tour-details/tour-details.component.ts
+++ src/app/components/tour-details/tour-details.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Tour } from 'src/app/model/tour';
+import { TourService } from 'src/app/services/tour.service';
 
 @Component({
   selector: 'app-tour-details',
   templateUrl: './tour-details.component.html',
   styleUrls: ['./tour-details.component.css']
 })
-export class TourDetailsComponent {
+export class TourDetailsComponent implements OnInit {
+  tour: Tour = new Tour();
+
+  constructor( private route: ActivatedRoute, private tourService: TourService) {}
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const slug = params['slug'];
+      // Use 'slug' to fetch tour details
+      this.tourService.getTourBySlug(slug).subscribe(response => {
+        this.tour = response;
+      });
+    });
+  }
+  
 
 }
