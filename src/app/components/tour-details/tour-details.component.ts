ac517a4b287b9bd667eab2fcbe615c82e70b79e4
Ishkevin32 <ishimwekevin32@gmail.com>
2024-01-24 07:54:12 +0200

    Data is now dynamically comming from the backend to the tour-details page

--- src/app/components/tour-details/tour-details.component.ts
+++ src/app/components/tour-details/tour-details.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Tour } from 'src/app/model/tour';
+import { TourService } from 'src/app/services/tour.service';
 
 @Component({
   selector: 'app-tour-details',
   templateUrl: './tour-details.component.html',
-  styleUrls: ['./tour-details.component.css']
+  styleUrls: ['./tour-details.component.css'],
 })
-export class TourDetailsComponent {
+export class TourDetailsComponent implements OnInit {
+  tour: Tour = new Tour();
 
+  constructor(
+    private route: ActivatedRoute,
+    private tourService: TourService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.params['id'];
+
+    this.tourService.getTourById(id).subscribe((response) => {
+      this.tour = response;
+      console.log(this.tour.guides);
+    });
+  }
 }
