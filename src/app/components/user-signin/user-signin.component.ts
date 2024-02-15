da37bab4bbabe3d729fa4682c7921f6cbc5231dc
Ishkevin32 <ishimwekevin32@gmail.com>
2024-02-15 09:12:45 +0200

    Logo link to the home page

--- src/app/components/user-signin/user-signin.component.ts
+++ src/app/components/user-signin/user-signin.component.ts
@@ -5,36 +5,32 @@ import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-user-signin',
   templateUrl: './user-signin.component.html',
-  styleUrls: ['./user-signin.component.css']
+  styleUrls: ['./user-signin.component.css'],
 })
 export class UserSigninComponent {
+  credentials = {
+    email: '',
+    password: '',
+  };
+  errorMessage: string = '';
+  constructor(private userservice: UserService, private router: Router) {}
+  onSubmit() {
+    this.userservice.login(this.credentials).subscribe(
+      (response) => {
+        // console.log('logged in successfully', response.data);
+        this.userservice.setUserEmail(response.data.user.email);
+        this.router.navigate(['/home']);
+      },
+      (error) => {
+        console.error('Logged in Fail', error);
 
-   credentials= {
-    email: "",
-   password: ""
-  }
- errorMessage: string = '';
- constructor(private userservice : UserService, private router: Router)
- {}
- onSubmit(){
-  this.userservice.login(this.credentials).subscribe(
-    (response) => 
-    {
-      // console.log('logged in successfully', response.data);
-      this.userservice.setUserEmail(response.data.user.email);
-      this.router.navigate(['/profile']);
-    },
-    (error) => {
-      console.error('Logged in Fail', error);
-      
-      if (error.status === 401) {
-        this.errorMessage = 'An unexpected error occurred.';
-      } else {
-        this.errorMessage = 'Signin failed. Wrong password. Try to Login Again';
-        
+        if (error.status === 401) {
+          this.errorMessage = 'An unexpected error occurred.';
+        } else {
+          this.errorMessage =
+            'Signin failed. Wrong password. Try to Login Again';
+        }
       }
-    }
-  );
-  
- }
+    );
+  }
 }
