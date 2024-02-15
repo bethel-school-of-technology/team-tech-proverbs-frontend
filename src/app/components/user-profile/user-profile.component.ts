9fd6d9e9489af121b11bbd217cf3714c4d8a47e4
Ishkevin32 <ishimwekevin32@gmail.com>
2024-02-16 00:51:27 +0200

    user can now update images and we also changed the nav profile tab

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/model/user';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent implements OnInit {
-
   submitPassword() {
     throw new Error('Method not implemented.');
   }
@@ -17,12 +17,12 @@ export class UserProfileComponent implements OnInit {
 
   isloggedIn: boolean = false;
   currentUser: any = {};
-  constructor (private uservice: UserService) {}
+  constructor(private uservice: UserService, private route: Router) {}
 
   ngOnInit(): void {
-    this.uservice.isloggedIn.subscribe(loggedIn => {
+    this.uservice.isloggedIn.subscribe((loggedIn) => {
       this.isloggedIn = loggedIn;
-      if(loggedIn) {
+      if (loggedIn) {
         const jwtString = localStorage.getItem('jwt');
         if (jwtString !== null) {
           const response = JSON.parse(jwtString);
@@ -33,27 +33,37 @@ export class UserProfileComponent implements OnInit {
       }
     });
   }
-  submitUserData(user: User): void  {
-    this.uservice.updateUserData(user).subscribe(response => {
+
+  submitUserData(user: User): void {
+    const formData = new FormData();
+    formData.append('name', user.name);
+    formData.append('email', user.email);
+    if (user.photo && typeof user.photo !== 'string') {
+      formData.append('photo', user.photo, user.photo.name); // Append the File object directly
+    }
+
+    this.uservice.updateUserData(formData).subscribe((response) => {
       this.currentUser = response.data.user;
-      // alert(response.data.user.name);
       this.resetToken(this.currentUser);
+      window.location.reload();
     });
   }
-  onPhotoChange(image: string) {
-    this.currentUser.photo = image;
-    alert(image);
+
+  onPhotoChange(event: any) {
+    const file = event.target.files[0];
+    this.currentUser.photo = file;
   }
+
   resetToken(user: any) {
     const jwtString = localStorage.getItem('jwt');
-        if (jwtString !== null) {
-          const response = JSON.parse(jwtString);
-          response.data.user = this.currentUser;
-          // alert("Updated Successful to:" + response.data.user.name);
-          localStorage.removeItem('jwt');
-          localStorage.setItem('jwt', JSON.stringify(response));
-        } else {
-          alert('user is not available');
-        }
+    if (jwtString !== null) {
+      const response = JSON.parse(jwtString);
+      response.data.user = this.currentUser;
+      // alert("Updated Successful to:" + response.data.user.name);
+      localStorage.removeItem('jwt');
+      localStorage.setItem('jwt', JSON.stringify(response));
+    } else {
+      alert('user is not available');
+    }
   }
 }
